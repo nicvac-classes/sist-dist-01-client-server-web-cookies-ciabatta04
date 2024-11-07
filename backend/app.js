c433373382c559e4606898699178ccf2c2a8215e
ciabatta04 <157687232+ciabatta04@users.noreply.github.com>
2024-11-07 15:58:39 +0000

    Consegna

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const res = require('express/lib/response');
 const app = express();
 
 // Configurazione EJS
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
-    res.render('form');
+    const name = req.cookies.name;
+    if (name){
+        res.render('greet', {message:'Bentornato', name: name});
+    }else{
+        res.render('form');
+    }
 });
 
 app.post('/greet', (req, res) => {
     const name = req.body.name;
-    res.render('greet', { name: name });
+    res.cookie('name', name, {maxAge: 24 * 60 * 60 * 1000});
+    res.render('greet', { message:'Benvenuto', name: name });
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+app.post('/logout', (req, res) => {
+    res.clearCookie('name');
+    res.redirect('/');
+});
+
